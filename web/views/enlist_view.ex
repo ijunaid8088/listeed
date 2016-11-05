a77ea4678a4a20d9c8fb763e81e03201b2e33308
Junaid Farooq <junaid@evercam.io>
2016-11-05 22:30:27 +0500

    Added hour view

--- web/views/enlist_view.ex
+++ web/views/enlist_view.ex
@@ -1,11 +1,11 @@
 defmodule Listeed.EnlistView do
   use Listeed.Web, :view
 
-  def render("show.json", %{enlist: enlist}) do
+  def render("show.json", %{image_count: enlist}) do
     %{data: render_one(enlist, Listeed.EnlistView, "enlist.json")}
   end
 
   def render("enlist.json", %{enlist: enlist}) do
-    %{id: enlist}
+    %{total_images: enlist}
   end
 end
